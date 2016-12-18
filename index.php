e8a1f8bb0c96d1b772fea3310129a19c660a4c8d
Wangjin <445899710@qq.com>
2016-12-18 20:50:23 +0800

    1
    1

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require_once dirname(__FILE__) . '/vendor/autoload.php';
+ 
+/* $loader = new Twig_Loader_String();
+$twig = new Twig_Environment($loader);
+ 
+echo $twig->render('Hello {{ name }}!', array('name' => 'Fabien')); */
+$loader = new Twig_Loader_Filesystem('./views');
+$twig = new Twig_Environment($loader, array(
+    /* 'cache' => './compilation_cache', */
+));
+ 
+echo $twig->render('index.html');
